237caab3cf0b5c206855c5bb473ce17e24e1eeeb
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-12-26 16:12:38 +0100

    add Google Analytics code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import ReactGA from 'react-ga';
+
+//Google Analytics code
+const ANALYTICS_KEY = process.env.REACT_APP_ANALYTICS_KEY; // Google analytics API key
+ReactGA.initialize(ANALYTICS_KEY);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
