eaebd0ce70ab31db9620a6026603708d67e2aa4a
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-12-27 13:13:21 +0100

    adjust active nav link color

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,26 +1,37 @@
 import React, {useState, useEffect} from 'react';
-import styled from 'styled-components';
 import {Navbar, Nav} from 'react-bootstrap';
+import styled from 'styled-components';
 
 import {SECTION_NAMES} from '../../data/constants';
 
 const NavigationBar = (props) => {
 
+    const StyledNavLink = styled(Nav.Link)`
+
+      &&&.active {
+        color: ${props => props.theme.themeColor} !important;
+      
+      }    
+`;
+
     return (
 
-            <Navbar bg="light" variant="light">
-                <Nav className="mr-auto">
-                    {
-                        SECTION_NAMES.map((section) => {
-                            return (
-                                <Nav.Link key={section.id} href={`#${section.id}`}>
-                                    {section.name}
-                                </Nav.Link>
-                            )
-                        })
-                    }
-                </Nav>
-            </Navbar>
+        <Navbar bg="light" variant="light">
+            <Nav className="mr-auto">
+                {
+                    SECTION_NAMES.map((section) => {
+                        return (
+                            <StyledNavLink key={section.id} href={`#${section.id}`}>
+
+                                {section.name}
+
+                            </StyledNavLink>
+
+                        )
+                    })
+                }
+            </Nav>
+        </Navbar>
 
     );
 };
