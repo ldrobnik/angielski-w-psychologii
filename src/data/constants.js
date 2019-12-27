7d3a9f4b640c48de87ffa828edef7327dfbb0912
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-12-27 09:39:14 +0100

    create file with global constants

---
+++ src/data/constants.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+//section names
+export const SECTION_NAMES = [
+    {
+        id: 'o-mnie',
+        name: 'O mnie'
+    },
+    {
+        id: 'angielski-w-psychologii',
+        name: 'Angielski w psychologii'
+    },
+    {
+        id: 'tlumaczenia-w-psychologii',
+        name: 'Tłumaczenia w psychologii'
+    },
+    {
+        id: 'oferta',
+        name: 'Oferta'
+    },
+    {
+        id: 'kontakt',
+        name: 'Kontakt'
+    }
+];
