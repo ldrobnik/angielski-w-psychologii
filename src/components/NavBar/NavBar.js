9451c079c488f95b6ed7c2756c1851753f7d037f
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-12-27 10:10:36 +0100

    add section links to Navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import {Navbar} from 'react-bootstrap';
 
+import {SECTION_NAMES} from '../../data/constants';
+
 const StyledWrapper = styled.div`
 
 `;
@@ -11,6 +13,15 @@ const NavBar = (props) => {
     return (
         <StyledWrapper>
             <Navbar>
+                {
+                    SECTION_NAMES.map((section) => {
+                        return (
+                            <a key={section.id} href={`#${section.id}`}>
+                                {section.name}
+                            </a>
+                        )
+                    })
+                }
             </Navbar>
         </StyledWrapper>
     );
