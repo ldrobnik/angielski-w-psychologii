561cab7ec4c76497971213a12d18c5e0123c4e66
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-12-27 19:44:18 +0100

    add responsive behaviour to navbar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -25,10 +25,36 @@ const GlobalStyle = createGlobalStyle`
 
 const Home = (props) => {
 
+    //window width at which side menu is changed to top menu
+    const mobileBreakpoint = 700;
+
+    // Specifies the current window width
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    // Updates the window width
+    const handleWindowSizeChange = () => {
+        setWindowWidth(window.innerWidth);
+    };
+
+    // Specifies whether the app is displayed on a mobile device
+    const isMobile = windowWidth <= mobileBreakpoint;
+
+    // Adds an event listener to monitor the screen width and remove the listener when the component unmounts
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowSizeChange);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        }
+    });
+
+    //display top menu on larger screens and side menu on small screens
+    const menu = isMobile ? "Sidebar" : <NavigationBar />;
+
     return (
         <React.Fragment>
             <GlobalStyle />
-            <NavigationBar />
+            {menu}
         </React.Fragment>
     );
 };
