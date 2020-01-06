5b7824e8429486ee14da7dc32a40c286252519e5
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-01-06 17:09:02 +0100

    adjust AnchorLink offset for mobile devices

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -3,7 +3,7 @@ import {Navbar, Nav} from 'react-bootstrap';
 import styled from 'styled-components';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
-import {SECTION_NAMES, OFFSET} from '../../data/constants';
+import {SECTION_NAMES, OFFSET_DESKTOP, OFFSET_MOBILE, MOBILE_BREAKPOINT} from '../../data/constants';
 
 import logo from '../../assets/images/logo.svg';
 
@@ -19,7 +19,29 @@ const NavigationBar = (props) => {
     // Specifies whether the nav is expanded
     const [navExpanded, setNavExpanded] = useState(false);
 
+    // Specifies the current window width
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
+    // Updates the window width
+    const handleWindowSizeChange = () => {
+        setWindowWidth(window.innerWidth);
+    };
+
+    // Specifies whether the app is displayed on a mobile device
+    const isMobile = windowWidth < MOBILE_BREAKPOINT;
+
+    //Specifies smooth scroll offset depending on the window width
+    const scrollOffset = isMobile ? OFFSET_MOBILE : OFFSET_DESKTOP;
+
+
+    // Adds an event listener to monitor the screen width and remove the listener when the component unmounts
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowSizeChange);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        }
+    });
 
     return (
 
@@ -34,7 +56,7 @@ const NavigationBar = (props) => {
             >
             <Navbar.Brand>
                 <AnchorLink href="#top"
-                            offset={OFFSET}
+                            offset={scrollOffset}
                             onClick={() => setNavExpanded(false)}
                 >
                     <img
@@ -55,7 +77,7 @@ const NavigationBar = (props) => {
                                 <StyledNavLink
                                     key={section.id}
                                     href={`#${section.id}`}
-                                    offset={OFFSET}
+                                    offset={scrollOffset}
                                     onClick={() => setNavExpanded(false)}
                                 >
 
