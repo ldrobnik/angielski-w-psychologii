2d32a6386deb445878b7ce3ebe9a174ca786445c
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-01-06 17:31:50 +0100

    replace styled-components GlobalStyle with scss

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,38 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import styled, {createGlobalStyle} from 'styled-components';
 
+import './Home.css';
 
 import NavigationBar from "../NavigationBar/NavigationBar";
 
 import {SECTION_NAMES, FILLER} from "../../data/constants";
 
-/* STYLED COMPONENTS */
-const GlobalStyle = createGlobalStyle`
-    body {
-        font-family: ${props => props.theme.serif};
-        color: ${props => props.theme.darkColor};
-        background: linear-gradient(${props => props.theme.themeColor}, ${props => props.theme.darkThemeColor});
-        min-height: 100vh;
-        }
-        
-    a {
-        text-decoration: none;
-        color: ${props => props.theme.darkColor};
-
-    }
-     
-     ::selection {
-        color: ${props => props.theme.lightColor};
-        background-color: ${props => props.theme.themeColor};
-     }
-`;
-
-
 const Home = (props) => {
 
     return (
         <div id="outer-container">
-            <GlobalStyle />
             <NavigationBar />
             <div id="top"></div>
             <div id="page-wrap">
