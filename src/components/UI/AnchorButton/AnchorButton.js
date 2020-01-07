1e333528c91616734bbb6bdea4fac6e6a359e807
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-01-07 18:08:04 +0100

    style AnchorButton

--- src/components/UI/AnchorButton/AnchorButton.js
+++ src/components/UI/AnchorButton/AnchorButton.js
@@ -1,41 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import {Button} from 'react-bootstrap';
 
 import './AnchorButton.css';
 
-import {OFFSET_DESKTOP, OFFSET_MOBILE, MOBILE_BREAKPOINT} from '../../../data/constants';
-
+import {OFFSET_DESKTOP} from '../../../data/constants';
 
 const AnchorButton = (props) => {
-
-    // Specifies the current window width
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-    // Updates the window width
-    const handleWindowSizeChange = () => {
-        setWindowWidth(window.innerWidth);
-    };
-
-    // Specifies whether the app is displayed on a mobile device
-    const isMobile = windowWidth < MOBILE_BREAKPOINT;
-
-    //Specifies smooth scroll offset depending on the window width
-    const scrollOffset = isMobile ? OFFSET_MOBILE : OFFSET_DESKTOP;
-
-
-    // Adds an event listener to monitor the screen width and remove the listener when the component unmounts
-    useEffect(() => {
-        window.addEventListener('resize', handleWindowSizeChange);
-
-        return () => {
-            window.removeEventListener('resize', handleWindowSizeChange);
-        }
-    });
 
     return (
             <AnchorLink
                 href={`#${props.target}`}
-                offset={scrollOffset}
+                offset={OFFSET_DESKTOP}
                 className="anchorButton">
                     {props.message}
             </AnchorLink>
