701a29172ddc21776b9998b0f0726b03b16a8e6b
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-01-09 21:48:53 +0100

    adjust offset for triggering animation in About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -43,7 +43,7 @@ const About = (props) => {
     };
 
     //offset for triggering animation - larger for mobile
-    const animationOffset = props.mobile ? "-60%" : "-40%";
+    const animationOffset = props.mobile ? "-50%" : "-40%";
     return (
         <React.Fragment>
 
