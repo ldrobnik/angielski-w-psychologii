d49491143aa3716d879329b9dbf606f8c4882dc0
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-01-11 17:56:33 +0100

    style spinner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import posed from "react-pose";
 import {connect} from 'react-redux';
-import './Home.css';
 
+import './Home.css';
 import NavigationBar from "../NavigationBar/NavigationBar";
 import TopBanner from '../TopBanner/TopBanner';
 import About from '../About/About';
@@ -9,32 +10,36 @@ import English from '../English/English';
 import Translations from '../Translations/Translations';
 import Offer from '../Offer/Offer';
 import Contact from '../Contact/Contact';
-import Spinner from '../UI/Spinner/Spinner';
 
+import Spinner from '../UI/Spinner/Spinner';
 import {SECTION_NAMES} from "../../data/constants";
+
 
 
 const Home = (props) => {
 
     //class applied to outer container to prevent scrolling before the page is loaded
-    const containerClass = (props.loaded) ? "siteContent" : "constrained siteContent";
+    const containerClass = (props.loaded) ? "" : "constrained";
 
-    //display spinner only if the page is not loaded
-    const spinner = (props.loaded) ? <div></div> : <Spinner />;
+    //display the navbar only after the page is loaded
+    const navBar = (props.loaded) ? <NavigationBar /> : null;
+
+    //display spinner only when the page is not loaded
+    const spinner = (!props.loaded) ? <Spinner /> : null;
 
     console.log(props.loaded, containerClass);
     return (
         <React.Fragment>
-            {spinner}
             <div className={containerClass}>
-                <NavigationBar/>
+                {spinner}
+                {navBar}
                 <div id="top"></div>
-                <TopBanner/>
-                <About id={SECTION_NAMES[0]}/>
-                <English id={SECTION_NAMES[1]}/>
-                <Translations id={SECTION_NAMES[2]}/>
-                <Offer id={SECTION_NAMES[3]}/>
-                <Contact id={SECTION_NAMES[4]}/>
+                <TopBanner />
+                <About id={SECTION_NAMES[0]} />
+                <English id={SECTION_NAMES[1]} />
+                <Translations id={SECTION_NAMES[2]} />
+                <Offer id={SECTION_NAMES[3]} />
+                <Contact id={SECTION_NAMES[4]} />
             </div>
         </React.Fragment>
     );
--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,49 +1,28 @@
 import React from 'react';
-import posed from 'react-pose';
-import {connect} from 'react-redux';
 
 import logoImage from '../../../assets/images/logo.svg';
-
-/* POSE */
-const AnimatedContent = posed.div({
-    visible: {
-        opacity: 1,
-        filter: 'blur(0px)'
-    },
-    hidden: {
-        opacity: 0,
-        filter: 'blur(20px)'
-    }
-});
-
+import './Spinner.css';
 
 const Spinner = (props) => {
 
-    //specifies whether the spinner is visible based on whether the page has loaded
-    const spinnerVisible = !props.loaded;
 
     return (
 
-        <AnimatedContent
-            pose={spinnerVisible ? 'visible' : 'hidden'}>
+        <div className="outerWrapper">
             <div className="spinnerWrapper">
 
                 <img
                     src={logoImage}
-                    alt='loading spinner'
+                    alt="loading spinner"
+                    height="110px"
+                    width="110px"
                 />
 
             </div>
             <div className="spinnerBackdrop"></div>
-        </AnimatedContent>
-
+        </div>
     );
 };
 
-const mapStateToProps = state => {
-    return {
-        loaded: state.pageLoaded
-    };
-};
 
-export default connect(mapStateToProps)(Spinner);
+export default Spinner;
