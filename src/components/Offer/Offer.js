e3b1df368c8d2309afe41dcd3044e4dc0f218c53
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-01-12 21:55:35 +0100

    add content to Offer section

--- src/components/Offer/Offer.js
+++ src/components/Offer/Offer.js
@@ -1,41 +1,142 @@
 import React, {useState, useEffect} from 'react';
-import {Row, Container} from 'react-bootstrap';
+import {connect} from 'react-redux';
+import {Row, Col, Container} from 'react-bootstrap';
+import posed from 'react-pose';
 import {Waypoint} from "react-waypoint";
 
 import './Offer.css';
 import {SECTION_NAMES, WEBSITE_TEXT} from "../../data/constants";
 import TextBubble from '../UI/TextBubble/TextBubble';
 
+/* POSE */
+
+const AnimatedMessageLeft = posed.div({
+    visible: {
+        transform: 'scale(1, 1)',
+        transition: {
+            type: 'spring',
+            stiffness: 100
+        }
+    },
+    hidden: {
+        transform: 'scale(0, 1)'
+    }
+});
+
+const AnimatedMessageRight = posed.div({
+    visible: {
+        transform: 'scale(1, 1)',
+        transition: {
+            type: 'spring',
+            stiffness: 100
+        }
+    },
+    hidden: {
+        transform: 'scale(0, 1)'
+    }
+});
+
 const Offer = (props) => {
 
+    //specifies whether text links should be visible
+    const [messagesVisible, setMessagesVisible] = useState(false);
+
+
+    //shows the messages
+    const showMessages = () => {
+        setMessagesVisible(true);
+    };
+
+    //offset for triggering animation - larger for mobile
+    const animationOffset = props.mobile ? "50px" : "100px";
+
+
     return (
-        <div className="sectionContent">
-            <Container>
-                <Row>
-                    <h1 id={SECTION_NAMES[3].id}>
-                        {SECTION_NAMES[3].name}
-                    </h1>
-                </Row>
-                <Row className="separator"></Row>
-                <Row>
-                    <TextBubble
-                        type="theme"
-                        className="offerBox"
-                    >
-                        <h2>{WEBSITE_TEXT.offer.english.title}</h2>
-                    </TextBubble>
-                </Row>
-                <Row>
-                    <TextBubble
-                        type="theme"
-                        className="offerBox"
-                    >
-                        <h2>{WEBSITE_TEXT.offer.translations.title}</h2>
-                    </TextBubble>
-                </Row>
-            </Container>
-        </div>
+        <React.Fragment>
+            <Waypoint
+                onEnter={() => showMessages()}
+                bottomOffset={animationOffset}
+            />
+            <div className="sectionContent">
+                <Container>
+                    <Row>
+                        <h1 id={SECTION_NAMES[3].id}>
+                            {SECTION_NAMES[3].name}
+                        </h1>
+                    </Row>
+                    <Row className="separator"></Row>
+                    <Row>
+                        <Col md={6}>
+                            <AnimatedMessageLeft
+                                pose={messagesVisible ? 'visible' : 'hidden'}
+                            >
+                                <TextBubble
+                                    type="theme"
+                                    className="offerBox"
+                                >
+                                    <Row>
+                                        <h2>{WEBSITE_TEXT.offer.english.title}</h2>
+                                    </Row>
+                                    <Row className="offerContent">
+                                        {WEBSITE_TEXT.offer.english.content.map((item, k) => {
+                                            return (
+                                                <div key={k}>
+                                                    <p className="serviceType">
+                                                        {item.type}
+                                                    </p>
+                                                    <p className="servicePrice">
+                                                        {item.price}
+                                                    </p>
+                                                </div>
+                                            );
+                                        })}
+                                    </Row>
+                                </TextBubble>
+                            </AnimatedMessageLeft>
+                        </Col>
+                        <Col>
+                            <AnimatedMessageRight
+                                pose={messagesVisible ? 'visible' : 'hidden'}
+                            >
+                                <TextBubble
+                                    type="theme"
+                                    className="offerBox"
+                                >
+                                    <Row>
+                                        <h2>{WEBSITE_TEXT.offer.translations.title}</h2>
+                                    </Row>
+                                    <Row className="offerContent">
+                                        {WEBSITE_TEXT.offer.translations.content.map((item, k) => {
+                                            return (
+                                                <div key={k}>
+                                                    <p className="serviceType">
+                                                        {item.type}
+                                                    </p>
+                                                    <p className="servicePrice">
+                                                        {item.price}
+                                                    </p>
+                                                </div>
+                                            );
+                                        })}
+                                    </Row>
+                                    <Row>
+                                        {WEBSITE_TEXT.offer.translations.footnote}
+                                    </Row>
+                                </TextBubble>
+                            </AnimatedMessageRight>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </React.Fragment>
     );
 };
 
-export default Offer;
+const mapStateToProps = state => {
+    return {
+        mobile: state.isMobile
+    };
+};
+
+
+export default connect(mapStateToProps)(Offer);
