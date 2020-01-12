a407f2cedc4dc8f6955844599af22132d291aa3a
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-01-12 11:40:25 +0100

    add illustrations to assets

--- src/components/English/English.js
+++ src/components/English/English.js
@@ -1,9 +1,31 @@
 import React, {useState, useEffect} from 'react';
 import {Row, Container} from 'react-bootstrap';
+import posed from 'react-pose';
 import './English.css';
 import TextBubble from '../UI/TextBubble/TextBubble';
 
 import {SECTION_NAMES, FILLER} from "../../data/constants";
+
+/* POSE */
+const AnimatedList = posed.div({
+    visible: {
+        delayChildren: 100,
+        staggerChildren: 350
+    }
+});
+
+const AnimatedMessage = posed.div({
+    visible: {
+        x: '0%',
+        transition: {
+            type: 'spring',
+            stiffness: 100
+        }
+    },
+    hidden: {
+        x: '300%'
+    }
+});
 
 const English = (props) => {
 
