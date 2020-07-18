32cac79efef4846ff335e5c27754a61a01d38624
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-07-18 10:14:36 +0200

    leave only facebook logo in facebook navbar link

--- src/data/constants.js
+++ src/data/constants.js
@@ -68,7 +68,7 @@ export const WEBSITE_TEXT = {
             name: <React.Fragment><FontAwesomeIcon
                 icon={faFacebookSquare}
                 className="facebookIcon"
-            />Facebook
+            />
             </React.Fragment>,
             url: 'https://www.facebook.com/Angielski-w-psychologii-111402360575036/'
         }
